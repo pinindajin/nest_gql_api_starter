b3d10d5820e54da5cf83028c63954bccf765cc51
“Jacob.McBride” <pinindajin@gmail.com>
2019-06-23 17:43:28 -0600

    typings d file

--- src/generate-typings-script.ts
+++ src/generate-typings-script.ts
@@ -4,7 +4,7 @@ import { join } from 'path';
 const graphQLDefinitionsFactory = new GraphQLDefinitionsFactory();
 graphQLDefinitionsFactory.generate({
   typePaths: ['./**/*.graphql'],
-  path: join(process.cwd(), 'src/graphql.ts'),
+  path: join(process.cwd(), 'src/graphql.schema.d.ts'),
   outputAs: 'class',
   // watch: true,
 });
