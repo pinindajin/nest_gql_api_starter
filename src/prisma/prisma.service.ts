5c53fba0ce7138ec4bf4764d4609d3f29a7ad0f0
“Jacob.McBride” <pinindajin@gmail.com>
2019-06-28 18:33:07 -0600

    e2e graphql server start

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -6,7 +6,7 @@ export class PrismaService extends Prisma {
   constructor() {
     super({
       // TODO: Env variable endpoint.
-      endpoint: 'http://localhost/4466',
+      endpoint: 'http://localhost:4466',
       debug: false,
     });
   }
