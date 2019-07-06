fbe21bfe34d6dd5f98aad100299535c87a8f0c89
“Jacob.McBride” <pinindajin@gmail.com>
2019-07-05 18:03:58 -0600

    wip

--- src/auth/strategy/google.strategy.ts
+++ src/auth/strategy/google.strategy.ts
@@ -1,16 +1,16 @@
 import { PassportStrategy } from '@nestjs/passport';
-import { Strategy } from 'passport-google-oauth20';
+import { Strategy as PassportGoogleOauth20Strategy } from 'passport-google-oauth20';
 import { ConfigKeyEnum, ConfigService } from '../../config/config.service';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
+export class GoogleStrategy extends PassportStrategy(PassportGoogleOauth20Strategy, 'google') {
 
   constructor(private readonly congigService: ConfigService) {
     super({
       clientID: congigService.get(ConfigKeyEnum.GOOGLE_OAUTH_CLIENT_ID),     // <- Replace this with your client id
       clientSecret: congigService.get(ConfigKeyEnum.GOOGLE_OAUTH_CLIENT_SECRET), // <- Replace this with your client secret
-      callbackURL: 'http://localhost:3000/auth/google/callback',
+      callbackURL: 'http://localhost:3030/auth/google/callback',
       passReqToCallback: true,
       scope: ['profile'],
     });
